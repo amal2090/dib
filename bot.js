5051a30083e3a2ce895164452a9848e693758dea
amal2090 <47703541+amal2090@users.noreply.github.com>
2019-02-22 00:56:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`..`,"http://twitch.tv/Dream")
+client.user.setGame(`Beyonce`,"http://twitch.tv/Dib")
 client.user.setStatus("dnd")
 });
 
